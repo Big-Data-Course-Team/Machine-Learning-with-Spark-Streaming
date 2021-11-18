a9d5abc9d9552ee8907f6ddc08ba63569b206457
Shruvi D <56840419+shruvid2@users.noreply.github.com>
2021-11-18 19:06:56 +0530

    K-Means clustering draft

---
+++ clustering/K-Means_cluster.py
@@ -0,0 +1,40 @@
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.evaluation import ClusteringEvaluator
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.feature import StandardScaler
+
+ # vector assembler transforms  a set of features into a single vector
+assemble=VectorAssembler(inputCols=['SENTIMENT','TWEET'], outputCol='features')
+
+
+#df  is the batch dataframe 
+assembled_data=assemble.transform(df)
+
+
+#standardizing the features column
+sc=StandardScaler(inputCol='features',outputCol='standardized')
+data_scale=sc.fit(assembled_data)
+output=data_sc.transform(assembled_data)
+#data_scale_output.show(2)
+
+silhouette_score=[]
+#Silhouette Score using ClusteringEvaluator() measures how close each point in one cluster is to points in the neighboring clusters
+evaluator = ClusteringEvaluator(predictionCol='prediction', featuresCol='standardized', \
+                                metricName='silhouette', distanceMeasure='squaredEuclidean')
+
+#Forming clusters of 2-10                            
+for i in range(2,10):
+    
+    KMeans_algo=KMeans(featuresCol='standardized', k=i)
+    
+    KMeans_fit=KMeans_algo.fit(output)
+    
+    output_kn=KMeans_fit.transform(output)
+    
+    
+    
+    score=evaluator.evaluate(output_km)
+    
+    silhouette_score.append(score)
+    
+    #print("Silhouette Score:",score)
