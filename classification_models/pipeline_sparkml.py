fdbd7bcde377d1fb7af45757f5bf7c4dc351b1fb
anushkahebbar <56372418+anushkahebbar@users.noreply.github.com>
2021-11-19 06:40:45 +0530

    Structure repo

--- classification_models/pipeline_sparkml.py
+++ classification_models/pipeline_sparkml.py
@@ -1,16 +1,18 @@
+from pyspark.ml import Pipeline
 from pyspark.ml.classification import LogisticRegression
 
 from pyspark.ml.feature import HashingTF, IDF, Tokenizer
 from pyspark.ml.feature import StringIndexer
-from pyspark.ml import Pipeline
-
 from pyspark.ml.feature import CountVectorizer
-
 from pyspark.ml.feature import NGram, VectorAssembler
 from pyspark.ml.feature import ChiSqSelector
 
-def build_trigrams(inputCol=["Tweet","Sentiment"], n=3):
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+def model_pipeline(inputCol=["Tweet","Sentiment"], n=3):
+    
     tokenizer = [Tokenizer(inputCol="Tweet", outputCol="words")]
+    
     ngrams = [
         NGram(n=i, inputCol="words", outputCol="{0}_grams".format(i))
         for i in range(1, n + 1)
@@ -21,26 +23,30 @@ def build_trigrams(inputCol=["Tweet","Sentiment"], n=3):
             outputCol="{0}_tf".format(i))
         for i in range(1, n + 1)
     ]
+    
     idf = [IDF(inputCol="{0}_tf".format(i), outputCol="{0}_tfidf".format(i), minDocFreq=5) for i in range(1, n + 1)]
 
     assembler = [VectorAssembler(
         inputCols=["{0}_tfidf".format(i) for i in range(1, n + 1)],
         outputCol="rawFeatures"
     )]
+    
     label_stringIdx = [StringIndexer(inputCol = "Sentiment", outputCol = "label")]
+    
     selector = [ChiSqSelector(numTopFeatures=2**14,featuresCol='rawFeatures', outputCol="features")]
+    
     lr = [LogisticRegression(maxIter=100)]
-    return Pipeline(stages=tokenizer + ngrams + cv + idf+ assembler + label_stringIdx+selector+lr)
+    
+    return Pipeline(stages = tokenizer + ngrams + cv + idf + assembler + label_stringIdx + selector + lr)
     
     
 #To run (?)
 '''
-pipeline=build_trigrams()
+pipeline = model_pipeline()
 model = pipeline.fit(train_set)
 predictions = model.transform(val_set)
 accuracy = predictions.filter(predictions.label == predictions.prediction).count() / float(val_set.count())
 
-from pyspark.ml.evaluation import BinaryClassificationEvaluator
 evaluator = BinaryClassificationEvaluator(rawPredictionCol="rawPrediction")
 
 roc_auc = evaluator.evaluate(predictions)
